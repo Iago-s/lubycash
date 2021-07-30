d175ed5dd0155ae2d990bbda03ab5f5af51a349a
iago-s <iagooliveira3845@hotmail.com>
2021-07-30 11:44:08 -0300

    feat: Filter extracts by date

--- api/app/Controllers/Http/AdminActionsController.ts
+++ api/app/Controllers/Http/AdminActionsController.ts
@@ -1,16 +1,77 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import { DateTime } from 'luxon';
+import Database from '@ioc:Adonis/Lucid/Database'
 
 import User from 'App/Models/User';
-import Extract from 'App/Models/Extract';
+
+interface DateFormat {
+  year: number;
+  month: number;
+  day: number;
+};
 
 export default class AdminExtractsController {
-  async getExtracts({ response, params }: HttpContextContract) {
+  async getExtracts({ request, response, params }: HttpContextContract) {
     const { id } = params;
+    const querys = request.qs();
+    var matchFrom, matchTo;
+    var extracts;
+
+    var dateFrom: DateFormat = {
+      year: 0,
+      month: 0,
+      day: 0,
+    };
+    var dateTo: DateFormat = {
+      year: 0,
+      month: 0,
+      day: 0,
+    };
 
     try {
       const user = await User.findByOrFail('id', id);
 
-      const extracts = await (await Extract.query()).filter(extract => extract.user_id === user.id);
+      if(querys?.from && querys?.to) {
+        matchFrom = querys?.from.match(/([0-9][0-9])\/([0-9][0-9])\/([0-9][0-9][0-9][0-9])/);
+        matchTo = querys?.to.match(/([0-9][0-9])\/([0-9][0-9])\/([0-9][0-9][0-9][0-9])/);
+
+        if(matchFrom?.length === 4 && matchTo?.length === 4) {
+          dateFrom.year = parseInt(matchFrom[3]);
+          dateFrom.month = parseInt(matchFrom[2]);
+          dateFrom.day = parseInt(matchFrom[1]);
+
+          dateTo.year = parseInt(matchTo[3]);
+          dateTo.month = parseInt(matchTo[2]);
+          dateTo.day = parseInt(matchTo[1]);
+
+          extracts =
+          await Database
+            .query()
+            .from('extracts')
+            .select('*')
+            .where('user_id', user.id)
+            .whereBetween(
+              'created_at',
+              [
+                DateTime.local(
+                  dateFrom.year,
+                  dateFrom.month,
+                  dateFrom.day
+                ).toSQLDate(),
+                DateTime.local(
+                  dateTo.year,
+                  dateTo.month,
+                  dateTo.day + 1,
+                ).toSQLDate()
+              ]
+            );
+          return response.json(extracts);
+        } else {
+          return response.json({ message: 'Formato de data errado' });
+        }
+      }
+
+      extracts = await Database.query().from('extracts').select('*').where('user_id', user.id);
 
       return response.json(extracts);
     } catch(err) {
